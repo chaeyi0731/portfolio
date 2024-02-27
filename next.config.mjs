952e7574e11313f2ffd7d8ff294fab5797dcb24e
ChaeYi <chaeyi990731@gmail.com>
2024-02-27 14:32:21 +0900

    :hammer: Modify: 이미지를 next.config 에 호스트 등록

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['stellatalk.s3.ap-northeast-2.amazonaws.com'],
+  },
+};
 
 export default nextConfig;
