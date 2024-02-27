75f50708019020a8e27a58d8ff3156019325e707
ChaeYi <chaeyi990731@gmail.com>
2024-02-27 19:49:48 +0900

    :art: Design: 디자인 변경 간격조절

--- src/components/TechStack/TechStackSection.tsx
+++ src/components/TechStack/TechStackSection.tsx
@@ -13,8 +13,8 @@ interface TechStackSectionProps {
 const TechStackSection: React.FC<TechStackSectionProps> = ({ tech }) => {
   return (
     <div className="flex flex-col justify-center">
-      <h2 className="text-xl font-bold">{tech.title}</h2>
-      <div className="text-gray-400">
+      <h2 className="text-2xl font-bold mt-9">{tech.title}</h2>
+      <div className="text-gray-400 mt-4">
         {tech.description.map((paragraph, index) => (
           <p key={index}>{paragraph}</p>
         ))}
