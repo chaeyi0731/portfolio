a9153685dd9ee550f9937ad00a7ad9a3f9a2e4d3
ChaeYi <chaeyi990731@gmail.com>
2024-02-27 16:41:21 +0900

    :recycle: Refactor: 프로필 사진과 정보 링크 모아둔 것을 컴포넌트화

---
+++ src/components/ProfileSection.tsx
@@ -0,0 +1,13 @@
+import Profileimage from './profileImage';
+import ContactInfo from './myInfo/ContactInfo';
+
+function ProfileSection() {
+  return (
+    <div className="flex flex-row 2xl:w-96 2xl:h-96 justify-center">
+      <Profileimage />
+      <ContactInfo />
+    </div>
+  );
+}
+
+export default ProfileSection;
