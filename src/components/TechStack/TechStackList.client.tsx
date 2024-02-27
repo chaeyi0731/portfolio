c0f6132070bac4fae74aefb9c1862a2391b5f2fb
ChaeYi <chaeyi990731@gmail.com>
2024-02-27 19:03:10 +0900

    :poop: Bad: 해당 코드가 클라이언트 코드로 되어야하기에 이름변경
    이름변경해도 usestate,useEffect 오류 해결못함

---
+++ src/components/TechStack/TechStackList.client.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import TechStackSection from './TechStackSection';
+
+function TechStackList() {
+  const [techStack, setTechStack] = useState([]);
+
+  useEffect(() => {
+    fetch('../StackJson/techStackData.json')
+      .then((response) => response.json())
+      .then((data) => setTechStack(data))
+      .catch((error) => console.error('json 불러오기 실패:', error));
+  }, []);
+
+  return (
+    <div className="tech-stack-list">
+      <h1>⚒️ 기술 스택</h1>
+      {techStack.map((tech, index) => (
+        <TechStackSection key={index} tech={tech} />
+      ))}
+    </div>
+  );
+}
+
+export default TechStackList;
