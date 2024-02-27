25c1211700e2f3e2d6f97c52e31502043e98fa55
ChaeYi <chaeyi990731@gmail.com>
2024-02-27 13:06:28 +0900

    :hammer: Modify: 간단하게 제목만 설정

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,3 +1,9 @@
-function Main() {}
+function Main() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-6xl font-bold text-center my-8">안녕하세요! 이채이입니다 👋</h1>
+    </div>
+  );
+}
 
 export default Main;
