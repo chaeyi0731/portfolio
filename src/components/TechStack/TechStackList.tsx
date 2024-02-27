2884256405dd1e64eb16021a11c5a0d5be025009
ChaeYi <chaeyi990731@gmail.com>
2024-02-27 17:34:05 +0900

    :art: Design: 기술스택 컴포넌트 작성

--- src/components/TechStack/TechStackList.tsx
+++ src/components/TechStack/TechStackList.tsx
@@ -1,5 +1,12 @@
 function TechStackList() {
-  return <div></div>;
+  return (
+    <div className="ml-56 flex justify-start flex-col">
+      <div>
+        <h1 className="text-2xl font-bold mt-7 ">⚒️기술스택</h1>
+        <h3>Javascript</h3>
+      </div>
+    </div>
+  );
 }
 
 export default TechStackList;
