b440fb06325906238f2330391ca7f9ce217f3502
ChaeYi <chaeyi990731@gmail.com>
2024-02-27 16:41:42 +0900

    :recycle: Refactor: 나의 정보 링크를 모아둔 컴포넌트

---
+++ src/components/myInfo/ContactInfo.tsx
@@ -0,0 +1,18 @@
+function ContactInfo() {
+  return (
+    <div className="flex flex-col ml-20">
+      <p className="font-bold">📧 Email</p>
+      <p className="mt-2.5">chaeyi990731@gmail.com</p>
+      <p className="font-bold mt-12">😺 GitHub</p>
+      <a className="mt-2.5 underline" href="https://github.com/chaeyi0731" target="_blank" rel="noopener noreferrer">
+        https://github.com/chaeyi0731
+      </a>
+      <p className="font-bold mt-12">📷 Instagram</p>
+      <a className="mt-2.5 underline" href="https://www.instagram.com/chae._.y99/" target="_blank" rel="noopener noreferrer">
+        https://www.instagram.com/chae._.y99/
+      </a>
+    </div>
+  );
+}
+
+export default ContactInfo;
