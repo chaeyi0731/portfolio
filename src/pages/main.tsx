43d87749482875167e3355e5b05856e24078fc53
ChaeYi <chaeyi990731@gmail.com>
2024-02-27 15:28:17 +0900

    :hammer: Modify: 스타일링과 p 태그 적용하여 글 작성

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,10 +1,17 @@
-import Profileimage from '@/components/profileImage';
+import Profileimage from '../components/profileImage';
 
 function Main() {
   return (
     <div className="container mx-auto">
       <h1 className="text-5xl font-bold text-center my-8">안녕하세요! 이채이입니다 👋</h1>
-      <Profileimage />
+      <div className="flex flex-row 2xl:w-96 2xl:h-96 justify-center">
+        <Profileimage />
+        <div className="flex flex-col ml-20 ">
+          <p className="font-bold">📧 Email</p>
+          <p className="mt-2.5">chaeyi990731@gmail.com</p>
+          <p className="font-bold mt-2.5">😺 GitHub</p>
+        </div>
+      </div>
     </div>
   );
 }
