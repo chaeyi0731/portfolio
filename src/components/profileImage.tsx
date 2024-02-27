0a6f2b463269adbb2a6781e9369bdaee38736062
ChaeYi <chaeyi990731@gmail.com>
2024-02-27 14:31:33 +0900

    :hammer: Modify: 이미지 파일을 컴포넌트로 작성하기

---
+++ src/components/profileImage.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+function Profileimage() {
+  return (
+    <Image
+      src="https://stellatalk.s3.ap-northeast-2.amazonaws.com/image/KakaoTalk_Photo_2024-02-25-21-38-35.jpeg"
+      alt="my profile Image"
+      width={500} // 이미지의 너비
+      height={300} // 이미지의 높이
+    />
+  );
+}
+
+export default Profileimage;
