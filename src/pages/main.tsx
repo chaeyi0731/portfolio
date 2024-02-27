614711dcd37d3f2aafeb45ad3fd8ebc34f443d3b
ChaeYi <chaeyi990731@gmail.com>
2024-02-27 14:32:38 +0900

    :hammer: Modify: 컴포넌트 불러오기

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,7 +1,10 @@
+import Profileimage from '@/components/profileImage';
+
 function Main() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-6xl font-bold text-center my-8">안녕하세요! 이채이입니다 👋</h1>
+      <h1 className="text-5xl font-bold text-center my-8">안녕하세요! 이채이입니다 👋</h1>
+      <Profileimage />
     </div>
   );
 }
