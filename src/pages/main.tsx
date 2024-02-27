6a4a3f3e04c299e0fe52c0012cac356e71418634
ChaeYi <chaeyi990731@gmail.com>
2024-02-27 17:34:30 +0900

    :art: Design: 디자인 변경

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -4,10 +4,10 @@ import Introduction from '../components/myInfo/Introduction';
 
 function Main() {
   return (
-    <div className="container mx-auto flex flex-col justify-center items-center overflow-y-hidden">
+    <div className="container mx-auto flex flex-col justify-center overflow-y-hidden">
       <h1 className="text-5xl font-bold text-center my-8">안녕하세요! 이채이입니다 👋</h1>
       <ProfileSection />
-      <hr className="border-white w-2/3 mt-10" />
+      <hr className="border-white w-2/3 mt-10 ml-56" />
       <Introduction />
       <TechStackList />
     </div>
