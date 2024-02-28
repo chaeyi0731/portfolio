71c138b5f2c95c70b6cc607057c7a8f7ad407dad
ChaeYi <chaeyi990731@gmail.com>
2024-02-28 15:14:23 +0900

    :hammer: Modify: 이벤트 핸들러 에러 처리

--- src/components/TechStack/TechStackList.client.tsx
+++ src/components/TechStack/TechStackList.client.tsx
@@ -4,18 +4,20 @@ import React, { useState, useEffect } from 'react';
 import TechStackSection from './TechStackSection';
 
 function TechStackList() {
-  const [techStack, setTechStack] = useState([]);
+  const [techStack, setTechStack] = useState<Tech[]>([]);
+
   const [selectedTechDescription, setSelectedTechDescription] = useState('');
 
   useEffect(() => {
     fetch('/data/mydata.json')
       .then((response) => response.json())
-      .then((data) => setTechStack(data))
+      .then((data: Tech[]) => setTechStack(data)) // Tech[] 타입으로 데이터를 처리
       .catch((error) => console.error('json 불러오기 실패:', error));
   }, []);
 
-  const handleTechClick = (description) => {
-    setSelectedTechDescription(description);
+  const handleTechClick = (description: string[]) => {
+    const descriptionText = Array.isArray(description) ? description.join(' ') : '';
+    setSelectedTechDescription(descriptionText);
   };
 
   return (
@@ -25,7 +27,7 @@ function TechStackList() {
         <div className="flex flex-wrap justify-between w-96 mr-10">
           {' '}
           {techStack.map((tech, index) => (
-            <TechStackSection key={index} tech={tech} onTechClick={() => handleTechClick(tech.description.join(' '))} />
+            <TechStackSection key={index} tech={tech} onTechClick={() => handleTechClick(tech.description)} />
           ))}
         </div>
         {selectedTechDescription && (
