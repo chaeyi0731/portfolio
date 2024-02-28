80caa9d714f78a394c16ce8366c69bc04740a077
ChaeYi <chaeyi990731@gmail.com>
2024-02-28 14:48:07 +0900

    :hammer: Modify:클릭이벤트 구현 형제 속성이기에 list 에서 json 을 불러옴

--- src/components/TechStack/TechStackList.client.tsx
+++ src/components/TechStack/TechStackList.client.tsx
@@ -4,6 +4,7 @@ import TechStackSection from './TechStackSection';
 
 function TechStackList() {
   const [techStack, setTechStack] = useState([]);
+  const [selectedTechDescription, setSelectedTechDescription] = useState('');
 
   useEffect(() => {
     fetch('/data/mydata.json')
@@ -12,13 +13,24 @@ function TechStackList() {
       .catch((error) => console.error('json 불러오기 실패:', error));
   }, []);
 
+  const handleTechClick = (description) => {
+    setSelectedTechDescription(description);
+  };
+
   return (
     <div className="flex ml-56 mt-14 flex-col">
-      <h1 className="text-2xl font-bold mt-7 ">⚒️ 기술 스택</h1>
-      <div className="tech-stack-list mt-14 flex  w-72 flex-wrap justify-between">
-        {techStack.map((tech, index) => (
-          <TechStackSection key={index} tech={tech} />
-        ))}
+      <h1 className="text-2xl font-bold mt-7">⚒️ 기술 스택</h1>
+      <div className="flex flex-row justify-between ">
+        <div className="tech-stack-list mt-14 w-72 flex flex-row flex-wrap justify-between">
+          {techStack.map((tech, index) => (
+            <TechStackSection key={index} tech={tech} onTechClick={() => handleTechClick(tech.description.join(' '))} />
+          ))}
+        </div>
+        {selectedTechDescription && (
+          <div className="description text-gray-400 mt-4 ">
+            <p>{selectedTechDescription}</p>
+          </div>
+        )}
       </div>
     </div>
   );
