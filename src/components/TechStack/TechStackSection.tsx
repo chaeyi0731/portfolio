78de0b09806cc9d9d511349e9530e0ce173c71a4
ChaeYi <chaeyi990731@gmail.com>
2024-02-28 17:19:03 +0900

    :art: Design: 글씨 크기 조절

--- src/components/TechStack/TechStackSection.tsx
+++ src/components/TechStack/TechStackSection.tsx
@@ -15,7 +15,7 @@ interface TechStackSectionProps {
 const TechStackSection: React.FC<TechStackSectionProps> = ({ tech, onTechClick }) => {
   return (
     <div className="tech-stack-section text-center" onClick={onTechClick}>
-      <h2 className="text-2xl font-bold mt-9">{tech.title}</h2>
+      <h2 className="text-xl font-bold mt-9">{tech.title}</h2>
       <img src={tech.imageUrl} alt={tech.title} className="cursor-pointer w-24" />
     </div>
   );
