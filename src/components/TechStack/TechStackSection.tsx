a82900c3e7b77053c09166d885ef58ba0f1f7f10
ChaeYi <chaeyi990731@gmail.com>
2024-02-28 14:21:54 +0900

    :poop: Bad: 설명은 별도의 div 에서 나오길

--- src/components/TechStack/TechStackSection.tsx
+++ src/components/TechStack/TechStackSection.tsx
@@ -1,9 +1,8 @@
-// TechStackSection.client.tsx
 import React, { useState } from 'react';
 
 interface Tech {
   title: string;
-  imageUrl: string; // 이미지 URL을 포함합니다.
+  imageUrl: string;
   description: string[];
 }
 
@@ -14,23 +13,21 @@ interface TechStackSectionProps {
 const TechStackSection: React.FC<TechStackSectionProps> = ({ tech }) => {
   const [showDescription, setShowDescription] = useState(false);
 
-  const toggleDescription = () => {
-    setShowDescription(!showDescription);
-  };
+  const toggleDescription = () => setShowDescription(!showDescription);
 
   return (
     <div className="tech-stack-section">
       <div className="image-container text-center">
         <h2 className="text-2xl font-bold mt-9">{tech.title}</h2>
-        <img src={tech.imageUrl} alt={tech.title} className="cursor-pointer w-24 " onClick={toggleDescription} />
-      </div>
-      {showDescription && (
-        <div className="text-gray-400 mt-4">
-          {tech.description.map((paragraph, index) => (
-            <p key={index}>{paragraph}</p>
-          ))}
+        <img src={tech.imageUrl} alt={tech.title} className="cursor-pointer w-24" onClick={toggleDescription} />
+        <div className="text-gray-400 mt-4" onClick={toggleDescription}>
+          {showDescription ? (
+            tech.description.map((paragraph, index) => <p key={index}>{paragraph}</p>)
+          ) : (
+            <p>설명 보기</p> // 클릭 전 기본 메시지
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
