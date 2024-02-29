146b6d6f5f08a3b340ce3244b7e392805a0a757c
ChaeYi <chaeyi990731@gmail.com>
2024-02-29 14:36:21 +0900

    :art: Design : 너비와 오른쪽 공백 늘리기

--- src/components/TechStack/TechStackSection.tsx
+++ src/components/TechStack/TechStackSection.tsx
@@ -1,6 +1,6 @@
 // TechStackSection.client.tsx
 import React from 'react';
-import { Tech } from '../interface/Tech'
+import { Tech } from '../interface/Tech';
 
 interface TechStackSectionProps {
   tech: Tech;
@@ -11,7 +11,7 @@ const TechStackSection: React.FC<TechStackSectionProps> = ({ tech, onTechClick }
   return (
     <div className="tech-stack-section text-center" onClick={onTechClick}>
       <h2 className="text-xl font-bold mt-9">{tech.title}</h2>
-      <img src={tech.imageUrl} alt={tech.title} className="cursor-pointer w-24" />
+      <img src={tech.imageUrl} alt={tech.title} className="cursor-pointer w-28 mr-10" />
     </div>
   );
 };
