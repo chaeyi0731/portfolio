26c086c56f25233c759bc650a668b1e5ae209c1d
ChaeYi <chaeyi990731@gmail.com>
2024-02-29 15:48:51 +0900

    :art: Design : 소개 추가 및 스크롤에 맞춰 크기 설정

--- src/components/myInfo/Introduction.tsx
+++ src/components/myInfo/Introduction.tsx
@@ -1,11 +1,12 @@
 function Introduction() {
   return (
-    <div className="ml-56 mt-32 flex flex-col justify-center">
+    <div className="ml-56 mt-64 flex flex-col justify-center">
       <h1 className="text-2xl font-bold mt-7 ">👨‍💻 Web Developer</h1>
       <ul className="mt-4 font-medium list-disc">
         <li className="mt-4 ml-5">
           안녕하세요! <span className="font-bold text-blue-400">꾸준히 성장하는 개발자 이채이</span> 입니다.
         </li>
+        <li className="mt-4 ml-5">소통과 배려를 좋아하기에 언제나 편안한 팀 분위기를 만들어 갈 수 있습니다</li>
         <li className="mt-4 ml-5">항상 생글생글 웃으며 힘든 작업, 과정이어도 웃으며 작업 할 수 있습니다.</li>
         <li className="mt-4 ml-5">무엇이든 관심이 생기면 빠르게 도전하고 실행합니다.</li>
         <li className="mt-4 ml-5">단 한 줄의 코드라도 서비스의 가치를 담으려고 노력합니다.</li>
