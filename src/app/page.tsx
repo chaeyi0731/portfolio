4f282ee3f14a593496168bca7fe35257e2766500
ChaeYi <chaeyi990731@gmail.com>
2024-02-29 11:02:10 +0900

    :poop: Bad: Header 전역적 표시
    _app.tsx로 바꿀예정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 // import Image from 'next/image';
+import Header from '@/components/Header/Header';
 import Main from '../pages/main';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Main />
-    </main>
+    <>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Main />
+      </main>
+    </>
   );
 }
