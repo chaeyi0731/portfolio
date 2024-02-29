be41dd6e2b7e7ceeb5ccb01a09555a7871a1a069
ChaeYi <chaeyi990731@gmail.com>
2024-02-29 15:08:58 +0900

    :art: Design : 헤더 위치 조정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,11 +12,14 @@ function Header({ profileRef, techStackRef, introRef }: HeaderProps) {
   };
 
   return (
-    <header className="fixed top-0 w-full h-28 flex justify-around text-xl bg-gray-400 bg-opacity-25">
-      <button onClick={() => scrollToSection(profileRef)}>Profile</button>
-      <button onClick={() => scrollToSection(introRef)}>intro</button>
-      <button onClick={() => scrollToSection(techStackRef)}>Skill</button>
-      <button onClick={() => scrollToSection(techStackRef)}>Project</button>
+    <header className="fixed top-0 w-full h-28 flex justify-between items-center text-xl bg-gray-400 bg-opacity-25">
+      <div className="ml-20">Chaeyi</div>
+      <div className="flex justify-around w-5/12">
+        <button onClick={() => scrollToSection(profileRef)}>Profile</button>
+        <button onClick={() => scrollToSection(introRef)}>intro</button>
+        <button onClick={() => scrollToSection(techStackRef)}>Skill</button>
+        <button onClick={() => scrollToSection(techStackRef)}>Project</button>
+      </div>
     </header>
   );
 }
