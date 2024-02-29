f957dd2cedc18ca2e4f68c1bbd5f6d8a00862fc6
ChaeYi <chaeyi990731@gmail.com>
2024-02-29 11:04:21 +0900

    :hammer: Modify: 글로벌 css 와 헤더 컴포넌트 불러오기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 // pages/_app.tsx
-import '../styles/globals.css'; // 글로벌 스타일
+import '../app/globals.css'; // 글로벌 스타일
 import type { AppProps } from 'next/app';
-// import Header from '../components/Header';
+import Header from '../components/Header/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Component {...pageProps} />
     </>
   );
