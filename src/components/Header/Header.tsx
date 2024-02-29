01ce7c262091b608e4a79a05072fb485b5140f27
ChaeYi <chaeyi990731@gmail.com>
2024-02-29 16:11:35 +0900

    :hammer: Modify: 타이틀 클릭하면 타이틀로 이동 스크롤 구현

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,16 +4,19 @@ interface HeaderProps {
   profileRef: RefObject<HTMLDivElement>;
   techStackRef: RefObject<HTMLDivElement>;
   introRef: RefObject<HTMLDivElement>;
+  titleRef: RefObject<HTMLDivElement>;
 }
 
-function Header({ profileRef, techStackRef, introRef }: HeaderProps) {
+function Header({ profileRef, techStackRef, introRef, titleRef }: HeaderProps) {
   const scrollToSection = (ref: RefObject<HTMLDivElement>) => {
     ref.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
     <header className="fixed top-0 w-full h-28 flex justify-between items-center text-xl bg-gray-400 bg-opacity-25">
-      <div className="ml-20">Chaeyi</div>
+      <div className="ml-20">
+        <button onClick={() => scrollToSection(titleRef)}>ChaeYi</button>
+      </div>
       <div className="flex justify-around w-5/12">
         <button onClick={() => scrollToSection(profileRef)}>Profile</button>
         <button onClick={() => scrollToSection(introRef)}>intro</button>
