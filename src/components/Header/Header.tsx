42536492ece7204880db4ea65cac5835bcfc2a74
ChaeYi <chaeyi990731@gmail.com>
2024-02-29 10:48:47 +0900

    :hammer: Modify: 헤더 스크롤 구현
    섹션을 나누어 해당 컴포넌트로 이동하는 스크롤을 구현 했다.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,39 @@
+import React, { useRef } from 'react';
+
+function Header() {
+  const infoRef = useRef<HTMLDivElement>(null);
+  const skillRef = useRef<HTMLDivElement>(null);
+  const projectRef = useRef<HTMLDivElement>(null);
+
+  const scrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
+    if (ref && ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <div>
+      <header className="flex justify-between p-4 bg-gray-200">
+        {/* 헤더 링크 */}
+        <button onClick={() => scrollToSection(infoRef)}>Info</button>
+        <button onClick={() => scrollToSection(skillRef)}>Skill</button>
+        <button onClick={() => scrollToSection(projectRef)}>Project</button>
+      </header>
+
+      {/* 페이지 컨텐츠 */}
+      <div className="container mx-auto overflow-y-hidden">
+        <section ref={infoRef} className="section">
+          Info Section
+        </section>
+        <section ref={skillRef} className="section">
+          Skill Section
+        </section>
+        <section ref={projectRef} className="section">
+          Project Section
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
