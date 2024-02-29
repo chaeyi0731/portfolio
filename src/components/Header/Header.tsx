ed7bf732425c3a6262d308b9c4d61dc829719d22
ChaeYi <chaeyi990731@gmail.com>
2024-02-29 14:51:05 +0900

    :hammer: Modify: intro 스크롤 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,10 @@ import React, { RefObject } from 'react';
 interface HeaderProps {
   profileRef: RefObject<HTMLDivElement>;
   techStackRef: RefObject<HTMLDivElement>;
+  introRef: RefObject<HTMLDivElement>;
 }
 
-function Header({ profileRef, techStackRef }: HeaderProps) {
+function Header({ profileRef, techStackRef, introRef }: HeaderProps) {
   const scrollToSection = (ref: RefObject<HTMLDivElement>) => {
     ref.current?.scrollIntoView({ behavior: 'smooth' });
   };
@@ -13,6 +14,7 @@ function Header({ profileRef, techStackRef }: HeaderProps) {
   return (
     <header className="fixed top-0 w-full h-28 flex justify-around text-xl bg-gray-400 bg-opacity-25">
       <button onClick={() => scrollToSection(profileRef)}>Profile</button>
+      <button onClick={() => scrollToSection(introRef)}>intro</button>
       <button onClick={() => scrollToSection(techStackRef)}>Skill</button>
       <button onClick={() => scrollToSection(techStackRef)}>Project</button>
     </header>
