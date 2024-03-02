92bd3d10ddab305c2b5ca6242e21b62c87869373
ChaeYi <chaeyi990731@gmail.com>
2024-03-02 17:48:33 +0900

    :art: Design: 스타일 테일윈드로 변경

--- src/components/Arrow/nextArrow.tsx
+++ src/components/Arrow/nextArrow.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { FaArrowRight } from 'react-icons/fa'; // react-icons 라이브러리 사용
 
-const NextArrow = ({ className, style, onClick }) => (
-  <div className={`${className} custom-next-arrow`} style={{ ...style, display: 'block' }} onClick={onClick}>
-    <FaArrowRight size={30} />
+const NextArrow = ({ onClick }) => (
+  <div className="custom-next-arrow text-right cursor-pointer" onClick={onClick}>
+    <FaArrowRight size={30} className="text-black" />
   </div>
 );
 
