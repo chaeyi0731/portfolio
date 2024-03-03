3778999ba59e2ee7ba2198f6d005176eaa9c27f0
ChaeYi <chaeyi990731@gmail.com>
2024-03-03 23:57:40 +0900

    :hammer: Modify: 이미지 도메인 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['stellatalk.s3.ap-northeast-2.amazonaws.com'],
+    domains: ['stellatalk.s3.ap-northeast-2.amazonaws.com', 'noticon-static.tammolo.com'],
   },
 };
 
