f0c1ebfa68a3d9bbca1cf3c6e3cc66c05898e2b1
ChaeYi <chaeyi990731@gmail.com>
2024-03-03 15:21:43 +0900

    :hammer: Modify: 이미지 클릭시 모달창 추가

---
+++ src/components/Modal/TechModal.tsx
@@ -0,0 +1,27 @@
+// TechModal.tsx
+import React from 'react';
+import { Tech } from '../interface/Tech';
+
+interface TechModalProps {
+  tech: Tech; // Tech 인터페이스 사용
+  onClose: () => void; // 모달 닫기 함수
+}
+
+const TechModal: React.FC<TechModalProps> = ({ tech, onClose }) => {
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-content">
+        <h2>{tech.title}</h2>
+        <img src={tech.imageUrl} alt={tech.title} />
+        <ul>
+          {tech.description.map((desc, index) => (
+            <li key={index}>{desc}</li>
+          ))}
+        </ul>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default TechModal;
