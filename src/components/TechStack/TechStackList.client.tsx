08320dec28c4720a9816a99824ec7f684445f3b6
ChaeYi <chaeyi990731@gmail.com>
2024-03-03 17:30:48 +0900

    :hammer: Modify: 스타일 변경

--- src/components/TechStack/TechStackList.client.tsx
+++ src/components/TechStack/TechStackList.client.tsx
@@ -1,8 +1,7 @@
 // TechStackList.client.tsx
 import React, { useState, useEffect } from 'react';
 import TechModal from '../Modal/TechModal';
-import TechStackSection from './TechStackSection'; // TechStackSection 컴포넌트를 임포트
-import { Tech } from '../interface/Tech';
+import TechStackSection from './TechStackSection';
 
 function TechStackList() {
   const [techStacks, setTechStacks] = useState({ frontend: [], backend: [], database: [], cloudServices: [], versionControl: [] });
@@ -39,11 +38,13 @@ function TechStackList() {
   }
 
   return (
-    <div>
+    <div className="mt-56 flex flex-row ">
       {['frontend', 'backend', 'database', 'cloudServices', 'versionControl'].map((category) => (
-        <div key={category} className="mb-4">
-          <h2 className="text-2xl font-bold mb-2">{category.charAt(0).toUpperCase() + category.slice(1)}</h2>
-          <div className="flex flex-wrap">
+        <div key={category} className="flex flex-col items-center mb-4 bg-white text-gray-700 w-60 mr-5 rounded-xl ">
+          <div>
+            <h2 className="text-2xl font-bold mb-20 mt-9 text-center">{category.charAt(0).toUpperCase() + category.slice(1)}</h2>
+          </div>
+          <div className="flex flex-wrap mb-20 items-center justify-center">
             {techStacks[category].map((tech, index) => (
               <TechStackSection key={index} tech={tech} onTechClick={() => handleTechClick(tech)} />
             ))}
