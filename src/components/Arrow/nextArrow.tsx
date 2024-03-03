a3dc94729ff8af0be203d1b17f3f55ef517f1348
ChaeYi <chaeyi990731@gmail.com>
2024-03-03 18:56:54 +0900

    :hammer: Modify: Prop 정의

--- src/components/Arrow/nextArrow.tsx
+++ src/components/Arrow/nextArrow.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { FaArrowRight } from 'react-icons/fa'; // react-icons 라이브러리 사용
+import { FaArrowRight } from 'react-icons/fa';
 
-const NextArrow = ({ onClick }) => (
+interface NextArrowProps {
+  onClick: (event: React.MouseEvent<HTMLDivElement>) => void;
+}
+
+const NextArrow: React.FC<NextArrowProps> = ({ onClick }) => (
   <div className="custom-next-arrow text-right cursor-pointer" onClick={onClick}>
     <FaArrowRight size={20} className="text-white" />
   </div>
