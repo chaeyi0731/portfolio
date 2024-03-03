cabb53dff4853bcbd014436540eb94cb16b8bceb
ChaeYi <chaeyi990731@gmail.com>
2024-03-03 18:57:10 +0900

    :hammer: Modify: img 가 아닌 next의 Image

--- src/components/Modal/TechModal.tsx
+++ src/components/Modal/TechModal.tsx
@@ -1,6 +1,7 @@
 // TechModal.tsx
 import React from 'react';
 import { Tech } from '../interface/Tech';
+import Image from 'next/image';
 
 interface TechModalProps {
   tech: Tech; // Tech 인터페이스 사용
@@ -12,7 +13,7 @@ const TechModal: React.FC<TechModalProps> = ({ tech, onClose }) => {
     <div className="modal-backdrop">
       <div className="modal-content">
         <h2>{tech.title}</h2>
-        <img src={tech.imageUrl} alt={tech.title} />
+        <Image src={tech.imageUrl} alt={tech.title} />
         <ul>
           {tech.description.map((desc, index) => (
             <li key={index}>{desc}</li>
