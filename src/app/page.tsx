d00edcc910244d3c137f3c09ada421f12c4cbd0e
ChaeYi <chaeyi990731@gmail.com>
2024-03-05 12:38:45 +0900

    :hammer: Modify: 헤더추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Main from '../pages/main';
-
+import Head from 'next/head';
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Main />
-    </main>
+    <>
+      <Head>
+        <title>꾸준히 발전하는 개발자 이채이</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Main />
+      </main>
+    </>
   );
 }
