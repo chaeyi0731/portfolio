b6fb1b5433c1e034e57ae75fa8b45d1bf61111f3
ChaeYi <chaeyi990731@gmail.com>
2024-03-05 12:38:57 +0900

    :art: Design: 스타일 이전으로 돌려놓기

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -3,7 +3,7 @@ import ContactInfo from './myInfo/ContactInfo';
 
 function ProfileSection() {
   return (
-    <div className="container flex flex-row 2xl:w-96 2xl:h-96 justify-center mt-64">
+    <div className="flex flex-row 2xl:w-96 2xl:h-96 justify-center mt-64">
       <Profileimage />
       <ContactInfo />
     </div>
