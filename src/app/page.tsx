642137922691e73f57d8bedfdf5ad39d39fe48c6
ChaeYi <chaeyi990731@gmail.com>
2024-03-13 11:54:43 +0900

    :art: Design: 메인만 보이도록 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Main from '../pages/main';
-import Head from 'next/head';
+
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-between p-24">
+    <>
       <Main />
-    </main>
+    </>
   );
 }
