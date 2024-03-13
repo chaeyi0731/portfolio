d7b2f8707254814337b635b58e1916463d6a928e
ChaeYi <chaeyi990731@gmail.com>
2024-03-13 11:54:58 +0900

    :art: Design: 크기조절

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -3,10 +3,10 @@ import ContactInfo from './myInfo/ContactInfo';
 
 function ProfileSection() {
   return (
-    <div className="container flex flex-row  justify-center h-screen">
+    <div className="container flex flex-row  justify-center  w-screen">
       <Profileimage />
       <ContactInfo />
-    </div> 
+    </div>
   );
 }
 
